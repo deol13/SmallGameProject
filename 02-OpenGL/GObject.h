876c996bdafd09a095aafa9985e3bf6a7f69b38a
Victor Martell <victorarias@live.se>
2015-04-14 17:15:15 +0200

    GObject
    Graphic Object + start av player-handler

---
+++ 02-OpenGL/GObject.h
@@ -0,0 +1,60 @@
+#ifndef GOBJECT_H
+#define GOBJECT_H
+
+#include <gl/glew.h>
+#include <gl/GL.h>
+
+#include "Vertex.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+class GObject
+{
+	private:
+		//Texture
+		GLuint gTexture;
+
+		//Buffers
+		GLuint gBuffer;
+		GLuint gAttribute;
+
+		//Vertex
+		std::vector<Vertex> vertices;
+		int nrOfVertices;
+
+		//Matrix
+		glm::mat4 scaleMatrix = glm::mat4(
+			);
+		glm::mat4 rotationMatrix = glm::mat4(
+			);
+		glm::mat4 translationMatrix = glm::mat4(
+			);
+
+		//
+		int drawMode;
+
+	public:
+		GObject();
+		GObject(std::vector<Vertex> vertices, int drawMode, GLuint gTexture);
+		~GObject();
+
+		void scale(float x, float y, float z); //set scale matrix
+		void rotate(float x, float y, float z); //set rotation matrix
+		void translate(float x, float y, float z); //set translation matrix
+
+		void loadObjectFile(std::string fileName);
+		void bindBuffers();
+
+		void render(); //Draw
+
+		void init();
+
+		void setTexture(GLuint gTexture);
+		GLuint getTexture();
+		void setVertices(std::vector<Vertex> vertices);
+		std::vector<Vertex> getVertices();
+};
+
+#endif
---
+++ 02-OpenGL/Player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+enum Direction {UP, DOWN, LEFT, RIGHT};
+
+class Player 
+{
+private:
+	int x, y;
+public:
+	Player();
+	attack();
+	move( Direction dir);
+	takeDamage
+};
+#endif
