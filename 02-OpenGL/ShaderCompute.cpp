b0992f390047713d041accf939fe2c9f77ce7c5b
Martin Nygren <martin_nygren9405@hotmail.com>
2015-06-10 13:05:24 +0200

    Synchen funkar

--- 02-OpenGL/ShaderCompute.cpp
+++ 02-OpenGL/ShaderCompute.cpp
@@ -1,24 +1,22 @@
-#include "ShaderParticle.h"
+#include "ShaderCompute.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-ShaderParticle::ShaderParticle(GLuint* gShaderP)
+ShaderCompute::ShaderCompute(GLuint* gShaderP)
 {
 	gShaderProgram = gShaderP;
 	compile();
-	ViewMatrix = glGetUniformLocation(*gShaderProgram, "ViewMatrix");
-	ProjectionMatrix = glGetUniformLocation(*gShaderProgram, "ProjectionMatrix");
 }
 
-ShaderParticle::~ShaderParticle()
+ShaderCompute::~ShaderCompute()
 {
 
 }
 
-bool ShaderParticle::compile()
+bool ShaderCompute::compile()
 {
-	const char* vertex_shader = R"(
+	const char* compute_shader = R"( 
 		#version 430
 
 		struct particles
@@ -36,126 +34,81 @@ bool ShaderParticle::compile()
 			particles data[];
 		};
 
-		out float pStopRender;
+		layout (local_size_x = 30, local_size_y = 1, local_size_z = 1)in;
 
-		void main () 
+		void main()
 		{
-			gl_Position = vec4(data[gl_VertexID].pos, 1);
-			pStopRender = data[gl_VertexID].stopRender;
-		}
-	)";
-
-	const char* geometry_shader = R"( 
-		#version 430
-		layout (points) in;
-		layout(triangle_strip, max_vertices = 4) out;
-
-		in float pStopRender[];
-
-		uniform mat4 ViewMatrix;
-		uniform mat4 ProjectionMatrix;
-		
-		layout (location = 0) out vec3 Positions;
-		
-		void main ()
-		{
-			if(pStopRender[0] > 0)
+			uint tmpID =  gl_GlobalInvocationID.x;
+			if( data[tmpID].life >= 60 )
 			{
-				Positions = gl_in[0].gl_Position.xyz;
-
-				gl_in[0].gl_Position = ViewMatrix * gl_in[0].gl_Position;
-
-				vec3 nPos = normalize(gl_in[0].gl_Position.xyz);
-				vec3 up = vec3(0.0f, 1.0f, 0.0f);
-				vec3 bBoardVec = cross(-nPos, up);
-				vec3 sizey = vec3(0.0f, 0.40f, 0.0f);
-				vec3 sizex = bBoardVec*0.30f;
-
-				//Triangle
-				//Upper left vertex(corner)
-				gl_Position = vec4( gl_in[0].gl_Position.xyz - sizex + sizey, 1.0f );
-				gl_Position = ProjectionMatrix * gl_Position;
-				EmitVertex();
-
-				//Lower left vertex(corner)
-				gl_Position = vec4( gl_in[0].gl_Position.xyz - sizex - sizey, 1.0f );
-				gl_Position = ProjectionMatrix * gl_Position;
-				EmitVertex();
-
-				//Upper right vertex(corner)
-				gl_Position = vec4( gl_in[0].gl_Position.xyz + sizex + sizey, 1.0f );
-				gl_Position = ProjectionMatrix * gl_Position;
-				EmitVertex();
-
-				//Lower right vertex(corner)
-				gl_Position = vec4( gl_in[0].gl_Position.xyz + sizex - sizey, 1.0f );
-				gl_Position = ProjectionMatrix * gl_Position;
-				EmitVertex();
-
-				EndPrimitive();
+				data[tmpID].pos -= data[tmpID].velocity;
+				data[tmpID].life -= 1;
 			}
+			data[tmpID].stopRender -= 1;
 		}
 	)";
 
-	const char* fragment_shader = R"(
-		#version 430
+	GLint success = 0;
 
-		layout (location = 0) in vec3 worldpos;
+	//create compute shader
+	GLuint cs = glCreateShader(GL_COMPUTE_SHADER);
+	glShaderSource(cs, 1, &compute_shader, nullptr);
+	glCompileShader(cs);
+	CompileErrorPrint(&cs);
 
-		layout (location = 0) out vec3 WorldPosOut;   
-		layout (location = 1) out vec3 DiffuseOut;     
-		layout (location = 2) out vec3 NormalOut;       
-
-		void main () {
-			WorldPosOut = worldpos;
-			DiffuseOut = vec3(1,0,0); //1,1,1
-			NormalOut = vec3(1,0,0);	
-		}
-	)";
-
-	GLint success;
-
-	//Vertex Shader
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, nullptr);
-	glCompileShader(vs);
-	CompileErrorPrint(&vs);
-
-	//Geometry Shader
-	GLuint gs = glCreateShader(GL_GEOMETRY_SHADER);
-	glShaderSource(gs, 1, &geometry_shader, nullptr);
-	glCompileShader(gs);
-	CompileErrorPrint(&gs);
-
-	//create fragment shader
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, nullptr);
-	glCompileShader(fs);
-	CompileErrorPrint(&fs);
-
+	//compute shader
 	*gShaderProgram = glCreateProgram();
-	glAttachShader(*gShaderProgram, vs);
-	glAttachShader(*gShaderProgram, gs);
-	glAttachShader(*gShaderProgram, fs);
+	glAttachShader(*gShaderProgram, cs);
 	glLinkProgram(*gShaderProgram);
-
 	LinkErrorPrint(gShaderProgram);
+
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
+		printf("Error");
 
 	return true;
 }
 
-void ShaderParticle::CompileErrorPrint(GLuint* shader)
+void ShaderCompute::LinkErrorPrint(GLuint* shaderProgram)
 {
-	GLint success = 0;
+	GLint success;
+	glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetProgramiv(*shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetProgramInfoLog(*shaderProgram, maxLength, &maxLength, &errorLog[0]);
+
+		std::fstream myfile;
+		myfile.open("errorLinkCheck.txt", std::fstream::out);
+		for (int i = 0; i < maxLength; i++)
+		{
+			myfile << errorLog[i];
+		}
+		myfile.close();
+
+		// Provide the infolog in whatever manor you deem best.
+		// Exit with failure.
+		glDeleteProgram(*shaderProgram); // Don't leak the shader.
+		throw;
+	}
+}
+
+void ShaderCompute::CompileErrorPrint(GLuint* shader)
+{
+	GLint success;
 	glGetShaderiv(*shader, GL_COMPILE_STATUS, &success);
 	if (success == GL_FALSE)
 	{
 		GLint maxLength = 0;
-		glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &maxLength);
+		glGetShaderiv((*shader), GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The maxLength includes the NULL character
 		std::vector<GLchar> errorLog(maxLength);
-		glGetShaderInfoLog(*shader, maxLength, &maxLength, &errorLog[0]);
+		glGetShaderInfoLog((*shader), maxLength, &maxLength, &errorLog[0]);
 
 		std::fstream myfile;
 		myfile.open("errorCheck.txt", std::fstream::out);
@@ -169,35 +122,5 @@ void ShaderParticle::CompileErrorPrint(GLuint* shader)
 		// Exit with failure.
 		glDeleteShader(*shader); // Don't leak the shader.
 		throw;
-	}
-}
-
-void ShaderParticle::LinkErrorPrint(GLuint* shaderProgram)
-{
-	GLint success = 0;
-	glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
-	if (success == GL_FALSE)
-	{
-		GLint maxLength = 0;
-		glGetProgramiv(*shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
-
-		// The maxLength includes the NULL character
-		std::vector<GLchar> errorLog(maxLength);
-		glGetProgramInfoLog(*shaderProgram, maxLength, &maxLength, &errorLog[0]);
-
-		std::fstream myfile;
-		myfile.open("errorCheck.txt", std::fstream::out);
-		for (int i = 0; i < maxLength; i++)
-		{
-			myfile << errorLog[i];
-		}
-		myfile.close();
-
-		// Provide the infolog in whatever manor you deem best.
-		// Exit with failure.
-		glDeleteProgram(*shaderProgram); // Don't leak the shader.
-
-		if (success == GL_FALSE)
-			throw;
 	}
 }
--- 02-OpenGL/ShaderParticle.cpp
+++ 02-OpenGL/ShaderParticle.cpp
@@ -1,22 +1,24 @@
-#include "ShaderCompute.h"
+#include "ShaderParticle.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-ShaderCompute::ShaderCompute(GLuint* gShaderP)
+ShaderParticle::ShaderParticle(GLuint* gShaderP)
 {
 	gShaderProgram = gShaderP;
 	compile();
+	ViewMatrix = glGetUniformLocation(*gShaderProgram, "ViewMatrix");
+	ProjectionMatrix = glGetUniformLocation(*gShaderProgram, "ProjectionMatrix");
 }
 
-ShaderCompute::~ShaderCompute()
+ShaderParticle::~ShaderParticle()
 {
 
 }
 
-bool ShaderCompute::compile()
+bool ShaderParticle::compile()
 {
-	const char* compute_shader = R"( 
+	const char* vertex_shader = R"(
 		#version 430
 
 		struct particles
@@ -34,81 +36,126 @@ bool ShaderCompute::compile()
 			particles data[];
 		};
 
-		layout (local_size_x = 30, local_size_y = 1, local_size_z = 1)in;
+		out float pStopRender;
 
-		void main()
+		void main () 
 		{
-			uint tmpID =  gl_GlobalInvocationID.x;
-			if( data[tmpID].life >= 60 )
-			{
-				data[tmpID].pos -= data[tmpID].velocity;
-				data[tmpID].life -= 1;
-			}
-			data[tmpID].stopRender -= 1;
+			gl_Position = vec4(data[gl_VertexID].pos, 1);
+			pStopRender = data[gl_VertexID].stopRender;
 		}
 	)";
 
-	GLint success = 0;
+	const char* geometry_shader = R"( 
+		#version 430
+		layout (points) in;
+		layout(triangle_strip, max_vertices = 4) out;
 
-	//create compute shader
-	GLuint cs = glCreateShader(GL_COMPUTE_SHADER);
-	glShaderSource(cs, 1, &compute_shader, nullptr);
-	glCompileShader(cs);
-	CompileErrorPrint(&cs);
+		in float pStopRender[];
 
-	//compute shader
+		uniform mat4 ViewMatrix;
+		uniform mat4 ProjectionMatrix;
+		
+		layout (location = 0) out vec3 Positions;
+		
+		void main ()
+		{
+			if(pStopRender[0] > 0)
+			{
+				Positions = gl_in[0].gl_Position.xyz;
+
+				gl_in[0].gl_Position = ViewMatrix * gl_in[0].gl_Position;
+
+				vec3 nPos = normalize(gl_in[0].gl_Position.xyz);
+				vec3 up = vec3(0.0f, 1.0f, 0.0f);
+				vec3 bBoardVec = cross(-nPos, up);
+				vec3 sizey = vec3(0.0f, 0.50f, 0.0f);
+				vec3 sizex = bBoardVec*0.50f;
+
+				//Triangle
+				//Upper left vertex(corner)
+				gl_Position = vec4( gl_in[0].gl_Position.xyz - sizex + sizey, 1.0f );
+				gl_Position = ProjectionMatrix * gl_Position;
+				EmitVertex();
+
+				//Lower left vertex(corner)
+				gl_Position = vec4( gl_in[0].gl_Position.xyz - sizex - sizey, 1.0f );
+				gl_Position = ProjectionMatrix * gl_Position;
+				EmitVertex();
+
+				//Upper right vertex(corner)
+				gl_Position = vec4( gl_in[0].gl_Position.xyz + sizex + sizey, 1.0f );
+				gl_Position = ProjectionMatrix * gl_Position;
+				EmitVertex();
+
+				//Lower right vertex(corner)
+				gl_Position = vec4( gl_in[0].gl_Position.xyz + sizex - sizey, 1.0f );
+				gl_Position = ProjectionMatrix * gl_Position;
+				EmitVertex();
+
+				EndPrimitive();
+			}
+		}
+	)";
+
+	const char* fragment_shader = R"(
+		#version 430
+
+		layout (location = 0) in vec3 worldpos;
+
+		layout (location = 0) out vec3 WorldPosOut;   
+		layout (location = 1) out vec3 DiffuseOut;     
+		layout (location = 2) out vec3 NormalOut;       
+
+		void main () {
+			WorldPosOut = worldpos;
+			DiffuseOut = vec3(1,0,0); //1,1,1
+			NormalOut = vec3(1,0,0);	
+		}
+	)";
+
+	GLint success;
+
+	//Vertex Shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, nullptr);
+	glCompileShader(vs);
+	CompileErrorPrint(&vs);
+
+	//Geometry Shader
+	GLuint gs = glCreateShader(GL_GEOMETRY_SHADER);
+	glShaderSource(gs, 1, &geometry_shader, nullptr);
+	glCompileShader(gs);
+	CompileErrorPrint(&gs);
+
+	//create fragment shader
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, nullptr);
+	glCompileShader(fs);
+	CompileErrorPrint(&fs);
+
 	*gShaderProgram = glCreateProgram();
-	glAttachShader(*gShaderProgram, cs);
+	glAttachShader(*gShaderProgram, vs);
+	glAttachShader(*gShaderProgram, gs);
+	glAttachShader(*gShaderProgram, fs);
 	glLinkProgram(*gShaderProgram);
-	LinkErrorPrint(gShaderProgram);
 
-	GLenum error = glGetError();
-	if (error != GL_NO_ERROR)
-		printf("Error");
+	LinkErrorPrint(gShaderProgram);
 
 	return true;
 }
 
-void ShaderCompute::LinkErrorPrint(GLuint* shaderProgram)
+void ShaderParticle::CompileErrorPrint(GLuint* shader)
 {
-	GLint success;
-	glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
-	if (success == GL_FALSE)
-	{
-		GLint maxLength = 0;
-		glGetProgramiv(*shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
-
-		// The maxLength includes the NULL character
-		std::vector<GLchar> errorLog(maxLength);
-		glGetProgramInfoLog(*shaderProgram, maxLength, &maxLength, &errorLog[0]);
-
-		std::fstream myfile;
-		myfile.open("errorLinkCheck.txt", std::fstream::out);
-		for (int i = 0; i < maxLength; i++)
-		{
-			myfile << errorLog[i];
-		}
-		myfile.close();
-
-		// Provide the infolog in whatever manor you deem best.
-		// Exit with failure.
-		glDeleteProgram(*shaderProgram); // Don't leak the shader.
-		throw;
-	}
-}
-
-void ShaderCompute::CompileErrorPrint(GLuint* shader)
-{
-	GLint success;
+	GLint success = 0;
 	glGetShaderiv(*shader, GL_COMPILE_STATUS, &success);
 	if (success == GL_FALSE)
 	{
 		GLint maxLength = 0;
-		glGetShaderiv((*shader), GL_INFO_LOG_LENGTH, &maxLength);
+		glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The maxLength includes the NULL character
 		std::vector<GLchar> errorLog(maxLength);
-		glGetShaderInfoLog((*shader), maxLength, &maxLength, &errorLog[0]);
+		glGetShaderInfoLog(*shader, maxLength, &maxLength, &errorLog[0]);
 
 		std::fstream myfile;
 		myfile.open("errorCheck.txt", std::fstream::out);
@@ -122,5 +169,35 @@ void ShaderCompute::CompileErrorPrint(GLuint* shader)
 		// Exit with failure.
 		glDeleteShader(*shader); // Don't leak the shader.
 		throw;
+	}
+}
+
+void ShaderParticle::LinkErrorPrint(GLuint* shaderProgram)
+{
+	GLint success = 0;
+	glGetProgramiv(*shaderProgram, GL_LINK_STATUS, &success);
+	if (success == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetProgramiv(*shaderProgram, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetProgramInfoLog(*shaderProgram, maxLength, &maxLength, &errorLog[0]);
+
+		std::fstream myfile;
+		myfile.open("errorCheck.txt", std::fstream::out);
+		for (int i = 0; i < maxLength; i++)
+		{
+			myfile << errorLog[i];
+		}
+		myfile.close();
+
+		// Provide the infolog in whatever manor you deem best.
+		// Exit with failure.
+		glDeleteProgram(*shaderProgram); // Don't leak the shader.
+
+		if (success == GL_FALSE)
+			throw;
 	}
 }
